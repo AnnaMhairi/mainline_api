83b8b1c80f768df7642c63b518583cbaf20b191a
AnnaMhairi <anna.m.macdonald@gmail.com>
2016-12-30 16:44:03 -0800

    Debugging

--- app/controllers/api_emails_controller.rb
+++ app/controllers/api_emails_controller.rb
@@ -2,5 +2,7 @@ class ApiEmailsController < ApplicationController
   def receive
     recipient = params[:email]
     Notifier.delay.welcome_email(recipient)
+    flash[:notice] = "Message has been sent!"
+    render :json => {:notice => flash[:notice]}
   end
 end
